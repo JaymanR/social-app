5533218247d06d2fb227953f9c734943be1e353b
Jayman Rana <jayman.code@gmail.com>
2026-02-26 02:05:22 -0500

    implement persistence for login

--- client/src/layout/nav/nav.ts
+++ client/src/layout/nav/nav.ts
@@ -10,15 +10,14 @@ import { AccountService } from '../../core/services/account-service';
 })
 
 export class Nav {
-  private accountService = inject(AccountService)
+  protected accountService = inject(AccountService)
   protected creds: any = {}
-  protected loggedIn = signal(false)
+
 
   login() {
     this.accountService.login(this.creds).subscribe({
       next: result => {
         console.log(result);
-        this.loggedIn.set(true);
         this.creds = {}
       },
       error: error => alert(error.message)
@@ -26,6 +25,6 @@ export class Nav {
   }
 
   logout() {
-    this.loggedIn.set(false);
+    this.accountService.logout();
   }
 }
