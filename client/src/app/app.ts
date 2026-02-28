e8f76abfa45e33a50b3c7ffae7641f7f6be1b823
JaymanR <jayman.code@gmail.com>
2026-02-28 00:20:03 -0500

    Implement routing and add new feature components for members, lists, and messages

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -3,12 +3,12 @@ import { Component, inject, OnInit, signal } from '@angular/core';
 import { lastValueFrom } from 'rxjs';
 import { Nav } from "../layout/nav/nav";
 import { AccountService } from '../core/services/account-service';
-import { Home } from "../features/home/home";
 import { User } from '../types/user';
+import { RouterOutlet } from "@angular/router";
 
 @Component({
   selector: 'app-root',
-  imports: [Nav, Home],
+  imports: [Nav, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
