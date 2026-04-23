146ff0876bb703af7e9ccdbaf7a761ea8b91a28f
Jayman Rana <jayman.code@gmail.com>
2026-04-22 23:52:22 -0400

    Add photo management functionality to member profiles

--- client/src/core/services/member-service.ts
+++ client/src/core/services/member-service.ts
@@ -5,7 +5,7 @@ import { EditableMember, Member, Photo } from '../../types/member';
 import { tap } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MemberService {
   private http = inject(HttpClient);
@@ -19,10 +19,10 @@ export class MemberService {
 
   getMember(id: string) {
     return this.http.get<Member>(this.baseUrl + 'members/' + id).pipe(
-      tap(member => {
-        this.member.set(member)
-      })
-    )
+      tap((member) => {
+        this.member.set(member);
+      }),
+    );
   }
 
   getMemberPhotos(id: string) {
@@ -31,5 +31,19 @@ export class MemberService {
 
   updateMember(member: EditableMember) {
     return this.http.put(this.baseUrl + 'members', member);
+  }
+
+  uploadPhoto(file: File) {
+    const formData = new FormData();
+    formData.append('file', file);
+    return this.http.post<Photo>(this.baseUrl + 'members/add-photo', formData);
+  }
+
+  setMainPhoto(photo: Photo) {
+    return this.http.put(this.baseUrl + 'members/set-main-photo/' + photo.id, {});
+  }
+
+  deletePhoto(photoId: number) {
+    return this.http.delete(this.baseUrl + 'members/delete-photo/' + photoId);
   }
 }
