10e8cbfb0a1eff60d841198b0a2bba8f0ba0795f
Jayman Rana <jayman.code@gmail.com>
2026-04-22 23:39:27 -0400

    Configure Cloudinary

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using API.Data;
 using API.Errors;
+using API.Helpers;
 using API.Interfaces;
 using API.Middleware;
 using API.Services;
@@ -22,6 +23,8 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 builder.Services.AddCors();
 builder.Services.AddScoped<ITokenService, TokenService>();
 builder.Services.AddScoped<IMemberRepository, MemberRepository>();
+builder.Services.Configure<CloudinarySettings>(builder.Configuration
+    .GetSection("CloudinarySettings"));
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
